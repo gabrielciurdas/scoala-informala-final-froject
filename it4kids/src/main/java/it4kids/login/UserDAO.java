6044e36cabc14935735d702564f89ddf56797ee6
Gabriel Ciurdas\Gabi <gabrielciurdas@gmail.com>
2017-03-14 21:42:24 +0200

    Login and add parent functionalities included. Project needs to be changed to Spring MVC.

---
+++ it4kids/src/main/java/it4kids/login/UserDAO.java
@@ -0,0 +1,209 @@
+package it4kids.login;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Gabi on 3/1/2017.
+ */
+public class UserDAO {
+
+	private int linesWritten = 0;
+	private String firstName = "";
+	private String accountType = "";
+	private String username = "";
+	
+	public boolean authenticateUser(String email, String password) {
+		boolean isValid = false;
+		try (Connection conn = newConnection("postgresql", "localhost", "5432", "it4kids", "postgres",
+				"aNewPa55w0rd");
+				Statement stm = conn.createStatement();
+				ResultSet rs = stm.executeQuery("select * from registered_users where email='" + email
+						+ "' and password='" + password + "'");) {
+			if (rs.next()) {
+				isValid = true;
+				firstName = rs.getString("first_name");
+				accountType = rs.getString("account_type");
+				username = rs.getString("userName");
+			} else {
+				isValid = false;
+			}
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return isValid;
+	}
+	
+	public int getUsernameId(String username) {
+		int id = 0;
+		try (Connection conn = newConnection("postgresql", "localhost", "5432", "it4kids", "postgres",
+				"aNewPa55w0rd");
+				Statement stm = conn.createStatement();
+				ResultSet rs = stm.executeQuery("select id from registered_users where username='" + username + "'");) {
+			if (rs.next()) {
+				id = rs.getInt("id");
+			} else {
+				System.out.println("username does not exist");
+			}
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return id;
+	}
+	
+	public boolean usernameAvailable(String username) {
+		boolean isAvailable = false;
+		try (Connection conn = newConnection("postgresql", "localhost", "5432", "it4kids", "postgres",
+				"aNewPa55w0rd");
+				Statement stm = conn.createStatement();
+				ResultSet rs = stm.executeQuery("select * from registered_users where username='" + username + "'");) {
+			if (rs.next()) {
+				isAvailable = false;
+			} else {
+				isAvailable = true;
+			}
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return isAvailable;
+	}
+
+	/**
+	 * This method writes a User object in the specified database by creating a
+	 * connection with a PostgreSQL server and using a query.
+	 *
+	 * @param user
+	 *            is the user to be written in the specified database.
+	 */
+
+	public void add(User user) {
+		try (Connection conn = newConnection("postgresql", "localhost", "5432", "it4kids", "postgres",
+				"aNewPa55w0rd");
+				PreparedStatement stm = conn
+						.prepareStatement("INSERT INTO registered_users(first_name, last_name, account_type, email, username, password, regdate)"
+								+ " values(?,?,?,?,?,?,CURRENT_TIMESTAMP)");) {
+
+			stm.setString(1, user.getFirstName());
+			stm.setString(2, user.getLastName());
+			stm.setString(3, user.getAccountType());
+			stm.setString(4, user.getEmail());
+			stm.setString(5, user.getUserName());
+			stm.setString(6, user.getPassword());
+			linesWritten = stm.executeUpdate();
+			
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void setChildId(int childId) {
+		try (Connection conn = newConnection("postgresql", "localhost", "5432", "it4kids", "postgres",
+				"aNewPa55w0rd");
+				PreparedStatement stm = conn
+						.prepareStatement("UPDATE parent SET id_child = ?" );) {
+
+			stm.setInt(1, childId);
+			//linesWritten = stm.executeUpdate();
+			stm.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void setParentId(int parentId) {
+		try (Connection conn = newConnection("postgresql", "localhost", "5432", "it4kids", "postgres",
+				"aNewPa55w0rd");
+				PreparedStatement stm = conn
+						.prepareStatement("INSERT INTO child(id_parent)" + " values(?)");) {
+
+			stm.setInt(1, parentId);
+			/*linesWritten = stm.executeUpdate();*/
+			stm.executeUpdate();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	/**
+	 * This method retrieves a list of user objects from the specified database
+	 * by creating a connection with a PostgreSQL server and using a query.
+	 *
+	 * @return the list of User objects.
+	 */
+	public List<User> getAll() {
+		List<User> result = new ArrayList<>();
+
+		try (Connection conn = newConnection("postgresql", "localhost", "5432", "it4kids", "postgres",
+				"aNewPa55w0rd");
+				Statement stm = conn.createStatement();
+				ResultSet rs = stm.executeQuery(
+						"select first_name, last_name, email, username, password, regdate" + " from registered_users");) {
+
+			while (rs.next()) {
+				User user = new User();
+
+				user.setFirstName(rs.getString("first_name"));
+				user.setLastName(rs.getString("last_name"));
+				user.setEmail(rs.getString("email"));
+				user.setUserName(rs.getString("username"));
+				user.setPassword(rs.getString("password"));
+				user.setDate(rs.getString("regdate").toString());
+
+				result.add(user);
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+
+		return result;
+	}
+
+	private static void loadDriver() {
+		try {
+			Class.forName("org.postgresql.Driver").newInstance();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			System.err.println("Can't load driver. Verify CLASSPATH");
+			System.err.println(e.getMessage());
+		}
+
+	}
+
+	private static Connection newConnection(String type, String host, String port, String dbName, String user,
+			String pw) {
+
+		loadDriver();
+		DriverManager.setLoginTimeout(60); // wait 1 min; optional: DB may be
+		// busy, good to set a higher
+		// timeout
+		try {
+			String url = new StringBuilder().append("jdbc:").append(type).append("://").append(host).append(":")
+					.append(port).append("/").append(dbName).append("?user=").append(user).append("&password=")
+					.append(pw).toString();
+			return DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			System.err.println("Cannot connect to the database: " + e.getMessage());
+		}
+
+		return null;
+	}
+	
+	public int getLinesWritten() {
+		return linesWritten;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getAccountType() {
+		return accountType;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+}
---
+++ it4kids/src/main/webapp/index.jsp
@@ -0,0 +1,38 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script type="text/javascript" src="fieldChecker.js"></script>
+<title>Login</title>
+</head>
+<body bgcolor="silver">
+	<form method="post" action="LoginServlet"
+		onsubmit="return checkLogin()">
+		<center>
+			<table border="0" width="30	%" cellpadding="4">
+				<thead>
+					<tr>
+						<th colspan="2">Pagina de login</th>
+					</tr>
+					<tr></tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Adresa de email</td>
+						<td><input type="text" name="email" value="" /></td>
+					</tr>
+					<tr>
+						<td>Parola</td>
+						<td><input type="password" name="password" value="" /></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td><input type="submit" value="Login" /></td>
+					</tr>
+				</tbody>
+			</table>
+		</center>
+	</form>
+</body>
+</html>
