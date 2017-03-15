237d302755043e44b1b6321abcd9387deede28c3
Gabriel Ciurdas\Gabi <gabrielciurdas@gmail.com>
2017-03-15 11:35:49 +0200

    Added ApplicationConfiguration class and spring dependencies. ArtifactiId has beeb changed to it4kids.

---
+++ it4kids/src/main/java/it4kids/configuration/ApplicationConfiguration.java
@@ -0,0 +1,12 @@
+
+/**
+ * Created by Gabi on 3/15/2017.
+ */
+package it4kids.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+
+}
