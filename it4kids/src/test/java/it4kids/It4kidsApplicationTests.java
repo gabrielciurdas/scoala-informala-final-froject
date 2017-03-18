3d0ba130edb83c555bb354eeb26d340a76cc1035
Gabriel Ciurdas\Gabi <gabrielciurdas@gmail.com>
2017-03-18 11:58:07 +0200

    it4kids.zip and src/test added

---
+++ it4kids/src/test/java/it4kids/It4kidsApplicationTests.java
@@ -0,0 +1,16 @@
+package it4kids;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class It4kidsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
