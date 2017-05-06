5723b9ffe5cfd36e3040eba4886b7ff3ff9a30ca
Gabriel Ciurdas\Gabi <gabrielciurdas@gmail.com>
2017-05-06 15:52:45 +0300

    removed duplicate files tue to an error cloning the repository

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: java -Dserver.port=$PORT -jar  target/it4kids-0.0.1-SNAPSHOT.jar
--Dspring.datasource.url
--- src/main/java/it4kids/dao/indatabase/quiz/AnswerDAO.java
+++
@@ -1,8 +0,0 @@
-package it4kids.dao.indatabase.quiz;
-
-import it4kids.dao.BaseDAO;
-import it4kids.domain.quiz.QuizAnswer;
-
-public interface AnswerDAO extends BaseDAO<QuizAnswer> {
-
-}
