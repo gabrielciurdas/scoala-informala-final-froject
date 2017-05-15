0c7936409606f6889b57776d2512aebb5b23662a
Gabriel Ciurdas\Gabi <gabrielciurdas@gmail.com>
2017-05-15 10:03:40 +0300

    Unit Tests added for ChildAccount and ParentAccount classes.

---
+++ it4kids/src/test/java/it4kids/domain/ParentAccountTest.java
@@ -0,0 +1,39 @@
+package it4kids.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import it4kids.domain.login.User;
+import it4kids.service.ValidationException;
+import it4kids.service.login.UserService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParentAccountTest {
+
+	@Autowired
+	private UserService userService;
+	
+	@Test(expected = ValidationException.class)
+	public void whenParentUsernameIsEmpty_ExceptionIsThrown() throws ValidationException {
+		String username = "";
+		
+		User user = new User();
+		user.setUserName(username);
+		
+		userService.validateParentUserName(user);
+	}
+	
+	@Test(expected = ValidationException.class)
+	public void whenParentUsernameIsComposedOfNonAlphaNumericalCharacters_ExceptionIsThrown() throws ValidationException {
+		String username = "#username%";
+		
+		User user = new User();
+		user.setUserName(username);
+		
+		userService.validateParentUserName(user);
+	}
+}
